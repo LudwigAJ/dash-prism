f4d0198eceb35f75861bdcb907b5a517c3a3a29c
LudwigAJ <ludwigalfred@gmail.com>
2026-05-10 15:39:53 +0100

    manually added tag

--- docs/conf.py
+++ docs/conf.py
@@ -16,6 +16,7 @@ extensions = [
 
 exclude_patterns = ["_build"]
 master_doc = "index"
+html_static_path = ["_static"]
 
 autodoc_default_options = {
     "members": True,
@@ -40,3 +41,10 @@ html_title = "Dash Prism"
 html_theme_options = {
     "navigation_with_keys": True,
 }
+html_js_files = [
+    (
+        "https://www.googletagmanager.com/gtag/js?id=G-WB80DBLQ68",
+        {"async": "async"},
+    ),
+    "js/google-analytics.js",
+]
